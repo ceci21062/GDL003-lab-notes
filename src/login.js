9b926e0a7c93e0c4f364552e8d46853056eba79a
ceci21062 <ceci21062@gmail.com>
2019-09-12 16:50:08 -0500

    login y logout con google y facebook

--- src/login.js
+++ src/login.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
-import "firebase";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+const firebaseKey = {
+    apiKey: "AIzaSyDlmbzfXjhd1R0VZf5p4-rcx5O17wxjXPI",
+    authDomain: "lap-note.firebaseapp.com",
+    databaseURL: "https://lap-note.firebaseio.com",
+    projectId: "lap-note",
+    storageBucket: "",
+    messagingSenderId: "571913348294",
+    appId: "1:571913348294:web:ed40ffc137e4aabcf763c7"
+  };
+
+firebase.initializeApp(firebaseKey)
 
 class LogIn extends Component {
     state={ isSignedIn: false}
+    uiConfig={
+        signInFlow: "popup",
+        signInOptions:[
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        ],
+        callbacks:{
+            signInSuccess:()=> false
+        }
+    }
+
+componentDidMount =()=>{
+    firebase.auth().onAuthStateChanged(user=>{
+        this.setState({isSignedIn: !!user })
+    })
+}
 render(){
 return (
   <div>
       {this.state.isSignedIn ? (
+        <span>
         <div>SIGNED IN!</div>
-      ) : (<div>NOT SIGNED IN!</div>
-      )}
+        <button onClick= {()=>firebase.auth().signOut()}>Sign Out</button>
+        </span>
+      ) : (
+     <StyledFirebaseAuth
+      uiConfig={this.uiConfig}
+      firebaseAuth={firebase.auth()}/>
+        )}
       </div>
       );
     }
